2002ce1c27ebc32fdaddcd86dd9fb4f5d1b4c3ef
Vikash Choudhary <120484001+VikashChoudhary-04@users.noreply.github.com>
2026-01-13 16:33:50 +0530

    Update dedupe.go

--- internal/utils/dedupe.go
+++ internal/utils/dedupe.go
@@ -1,1 +1,15 @@
 package utils
+
+func Dedupe(input []string) []string {
+	seen := make(map[string]bool)
+	var result []string
+
+	for _, i := range input {
+		if !seen[i] {
+			seen[i] = true
+			result = append(result, i)
+		}
+	}
+
+	return result
+}
