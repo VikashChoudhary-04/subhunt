2ca9715381c4525a91709389c8e1ceda4a47ee58
Vikash Choudhary <120484001+VikashChoudhary-04@users.noreply.github.com>
2026-01-13 16:32:57 +0530

    Implement hostname resolution with timeout
    Add a function to resolve hostnames with a timeout.

--- internal/resolver/resolver.go
+++ internal/resolver/resolver.go
@@ -1,1 +1,23 @@
 package resolver
+
+import (
+	"context"
+	"net"
+	"time"
+)
+
+func Resolve(subs []string) []string {
+	var alive []string
+
+	for _, s := range subs {
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+
+		ips, err := net.DefaultResolver.LookupHost(ctx, s)
+		if err == nil && len(ips) > 0 {
+			alive = append(alive, s)
+		}
+	}
+
+	return alive
+}
