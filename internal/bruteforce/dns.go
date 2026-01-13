ad298945e4ffcfcfce146f3fa33b735470917e57
Vikash Choudhary <120484001+VikashChoudhary-04@users.noreply.github.com>
2026-01-13 16:32:17 +0530

    Add Brute function for DNS brute forcing
    Implement brute force DNS lookup using a wordlist.

--- internal/bruteforce/dns.go
+++ internal/bruteforce/dns.go
@@ -1,1 +1,28 @@
 package bruteforce
+
+import (
+	"bufio"
+	"net"
+	"os"
+)
+
+func Brute(domain, wordlist string) []string {
+	file, err := os.Open(wordlist)
+	if err != nil {
+		return nil
+	}
+	defer file.Close()
+
+	var found []string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		sub := scanner.Text() + "." + domain
+		_, err := net.LookupHost(sub)
+		if err == nil {
+			found = append(found, sub)
+		}
+	}
+
+	return found
+}
